fc8ad206d17429204592993bf4821722ee5dd97b
martinasartori97 <martinasartori9@gmail.com>
2024-10-04 15:20:34 +0200

    edit js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // prova collegamento javascript
 console.log("hello");
 
+ // palidroma
  //Chiedo all’utente di inserire una parola
 const word = prompt("inserisci una parola");
 console.log(word);
 
-
 // creo la funzione per verificare se la parola inserita è palindroma
-function is_palindroma(word)
+function ispalindroma(word)
+
+
+
+
+
+
+
+
+//pari e dispari
+// l'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// chiedo all'utente se sceglie pari o dispari
