e757fcbc4f3dc41132f97abd870a135fe8b12bef
martinasartori97 <martinasartori9@gmail.com>
2024-10-04 14:30:14 +0200

    edit js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // prova collegamento javascript
 console.log("hello");
+
+ //Chiedo all’utente di inserire una parola
+const word = prompt("inserisci una parola");
+console.log(word);
+
+
+// creo la funzione per verificare se la parola inserita è palindroma
+function ispalindroma(word)
